84e0bac4e50e09929cfbafeb9dd71f4269717793
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 06:22:17 -0700

    different method

--- stop.sh
+++ stop.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-set -e
-
-lsof -ti:3001 | xargs -r kill
+pkill -f "node.*server"
 sleep 1
-echo "Server 1 stopped"
-
-lsof -ti:3002 | xargs -r kill
+pkill -f "node.*client" 
 sleep 1
-echo "Server 2 stopped"
-
-sleep 2
 echo "Environment stopped"
