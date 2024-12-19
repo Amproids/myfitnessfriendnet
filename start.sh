42e9f5f93d74eb58e63914a5b51d4a4777e02908
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 05:55:06 -0700

    added more &

--- start.sh
+++ start.sh
@@ -12,12 +12,11 @@ echo "Starting  servers..."
 # Start server
 cd server
 npm install
-npm run start &
+npm run start &&
 
 # Start client
 cd ../client
 npm install
-npm run build &
+npm run build &&
 
 echo "Dev environment ready"
-reset
