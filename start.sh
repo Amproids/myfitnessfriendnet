a5749f943e7693071d1857b639790a0887dd29e3
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 06:10:56 -0700

    more waiting

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ chmod 775 stop.sh
 
 echo "Starting servers..."
 (cd server && npm install && npm run start) &
+wait
 (cd client && npm install && npm run build) &
-
 wait
 echo "Client started"
--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 (lsof -ti:3001 | xargs -r kill) &
+wait
 (lsof -ti:3002 | xargs -r kill) &
 wait
 echo "Environment stopped"
