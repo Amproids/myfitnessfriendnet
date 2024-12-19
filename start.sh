5dc5e6ad394a0f1f0dc0035863297aeee4e7443f
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 06:12:25 -0700

    sleep killing

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 git fetch origin && git reset --hard origin/main
 chmod 775 start.sh
 chmod 775 stop.sh
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-(lsof -ti:3001 | xargs -r kill) &
-wait
-(lsof -ti:3002 | xargs -r kill) &
-wait
+lsof -ti:3001 | xargs -r kill
+sleep 1
+lsof -ti:3002 | xargs -r kill
+sleep 1
 echo "Environment stopped"
