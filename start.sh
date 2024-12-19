d467f7425d270be440bc90ff7ee1e8f6225a316f
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 06:13:59 -0700

    more sleep

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ chmod 775 stop.sh
 
 echo "Starting servers..."
 (cd server && npm install && npm run start) &
+sleep 3
+echo "Server started"
 (cd client && npm install && npm run build) &
-sleep 1
+sleep 3
 echo "Client started"
