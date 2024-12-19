560796c14827fb09a6e18c5632e62c4fdade767a
Andrew Parry <parryandrew47@gmail.com>
2024-12-18 22:18:51 -0700

    added beep to update for testing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/webhook', async (req, res) => {
         try {
             const { exec } = require('child_process');
             // Execute pull and npm install
-            exec('pkill -f "node server.js" && git pull && ./start.sh', (error, stdout, stderr) => {
+            exec('play -n synth 0.1 sine 1000 && pkill -f "node server.js" && git pull && ./start.sh && play -n synth 0.1 sine 1000', (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Error: ${error}`);
                     return res.status(500).send('Deploy failed');
