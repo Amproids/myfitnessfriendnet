ff7a59f4367dc1e2e22d82704ec3305d43aa3c09
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 00:14:15 -0700

    test 17

--- fitness-server.js
+++ fitness-server.js
@@ -7,34 +7,34 @@ app.use(express.json());
 
 //root route
 app.get('/', (req, res) => {
-  res.json({ message: 'Webhook test 16. Hi Anja, my love! :) <3' });
+    res.json({ message: 'Webhook test 17. Hi Anja, my love! :) <3' });
 });
 
 //base page route
 
 //Webhook endpoint
 app.post('/webhook', async (req, res) => {
-  if (req.body.ref === 'refs/heads/main') {
-    try {
-      const { exec } = require('child_process');
-      res.status(200).send('Deploy initiated');
-      exec('./webhook.sh', 
-        (error, stdout) => {
-          if (error && !error.message.includes('pkill')) {
-            console.error(`Error: ${error}`);
-            return res.status(500).send('Deploy failed');
-          }
-          console.log(stdout);
-          res.status(200).send('Deployed successfully');
+    if (req.body.ref === 'refs/heads/main') {
+        try {
+            const { exec } = require('child_process');
+            res.status(200).send('Deploy initiated');
+            exec('./webhook.sh', 
+                (error, stdout) => {
+                    if (error && !error.message.includes('pkill')) {
+                        console.error(`Error: ${error}`);
+                        return res.status(500).send('Deploy failed');
+                    }
+                console.log(stdout);
+                res.status(200).send('Deployed successfully');
+                }
+            );
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(`${error}\nDeploy failed'`);
         }
-      );
-    } catch (error) {
-      console.error(error);
-      res.status(500).send(`${error}\nDeploy failed'`);
+    } else {
+        res.status(200).send('Not main branch');
     }
-  } else {
-    res.status(200).send('Not main branch');
-  }
 });
   
 app.listen(port, () => {
