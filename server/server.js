fc82e6dadd35526d5f5347a815f7e604dbac41f5
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 04:57:32 -0700

    if

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   });
 
-
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port: ${port}`);
 });
