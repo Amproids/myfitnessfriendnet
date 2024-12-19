09484192fad7d285208439973fda16d36824bccc
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 05:22:39 -0700

    stop update

--- stop.sh
+++ stop.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-
-# Kill process on port 3001 (server)
+# Kill only our specific server processes
 SERVER_PID=$(lsof -ti:3001)
-if [ ! -z "$SERVER_PID" ]; then
-    kill $SERVER_PID
-    echo "Server stopped (port 3001)"
-fi
-
-# Kill process on port 3002 (client)
 CLIENT_PID=$(lsof -ti:3002)
-if [ ! -z "$CLIENT_PID" ]; then
-    kill $CLIENT_PID
-    echo "Client stopped (port 3002)"
-fi
+
+[ ! -z "$SERVER_PID" ] && kill $SERVER_PID
+[ ! -z "$CLIENT_PID" ] && kill $CLIENT_PID
+
+echo "Services stopped"
