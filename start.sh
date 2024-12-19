7bf64d14d455beff4247a254b209b62267d0b548
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 06:14:59 -0700

    deaonf

--- start.sh
+++ start.sh
@@ -4,9 +4,10 @@ chmod 775 start.sh
 chmod 775 stop.sh
 
 echo "Starting servers..."
-(cd server && npm install && npm run start) &
+cd server && npm install && npm run start &
 sleep 3
 echo "Server started"
-(cd client && npm install && npm run build) &
+
+cd client && npm install && npm run build &
 sleep 3
 echo "Client started"
