8a4b2dd6dab1abd6e54d09348c88c0917b12ea5c
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 04:52:12 -0700

    typo fix

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, '../dist'))); 
+app.use(express.static(path.join(__dirname, '../client/dist'))); 
 
 // Static Routes
 app.get('/', (req, res) => {
