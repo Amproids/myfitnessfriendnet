02a6c2a96261aeb2c5bca2c84bad1640c5aa5115
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 06:17:41 -0700

    ckenawo

--- stop.sh
+++ stop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
+
 lsof -ti:3001 | xargs -r kill
-sleep 3
+sleep 1
+echo "Server 1 stopped"
+
 lsof -ti:3002 | xargs -r kill
-sleep 3
+sleep 1
+echo "Server 2 stopped"
+
+sleep 2
 echo "Environment stopped"
