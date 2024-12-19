554a6528041f92e8487524e137892acbdbdede97
Andrew Parry <parryandrew47@gmail.com>
2024-12-18 23:54:15 -0700

    test 11

--- fitness-server.js
+++ fitness-server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 //root route
 app.get('/', (req, res) => {
-  res.json({ message: 'Webhook test 10. Hi Anja, my love! :) <3' });
+  res.json({ message: 'Webhook test 11. Hi Anja, my love! :) <3' });
 });
 
 //base page route
