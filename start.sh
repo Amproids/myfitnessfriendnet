cfc044a580117c4f2079496d03b84de43167f04b
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 06:09:59 -0700

    idk

--- start.sh
+++ start.sh
@@ -2,12 +2,11 @@
 set -e
 git fetch origin && git reset --hard origin/main
 chmod 775 start.sh
+chmod 775 stop.sh
 
 echo "Starting servers..."
 (cd server && npm install && npm run start) &
 (cd client && npm install && npm run build) &
 
 wait
-sleep 1
-reset
 echo "Client started"
--- stop.sh
+++ stop.sh
@@ -2,6 +2,4 @@
 (lsof -ti:3001 | xargs -r kill) &
 (lsof -ti:3002 | xargs -r kill) &
 wait
-sleep 1
-reset
 echo "Environment stopped"
