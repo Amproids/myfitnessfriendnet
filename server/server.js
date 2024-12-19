d1bc826a75aaa860870654ec0e5cd0311cfa0e2f
Andrew Parry <parryandrew47@gmail.com>
2024-12-19 07:00:38 -0700

    hmm

--- server/server.js
+++ server/server.js
@@ -24,7 +24,6 @@ app.listen(port, '0.0.0.0', () => {
 const { exec } = require('child_process');
 
 app.post('/webhook', (req, res) => {
-    console.log('Webhook received');
     res.sendStatus(200);
     
     const command = 'pm2 restart all';
